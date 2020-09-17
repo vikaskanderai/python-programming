098956abc1c47103172b6b0c34091f5618fa27ef
vikaskanderai <71124571+vikaskanderai@users.noreply.github.com>
2020-09-17 13:24:45 +0530

    Add files via upload
    This is a program which accepts integer inputs and prints the positive numbers in it.

---
+++ positive numbers.py
@@ -0,0 +1,17 @@
+print("Enter 3 number")
+num1=int(input("Enter 1st number:- "))
+num2=int(input("Enter 2nd number:- "))
+num3=int(input("Enter 3rd number:- "))
+list_a=[]
+list_a.append(num1)
+list_a.append(num2)
+list_a.append(num3)
+print("The input list is ",list_a)
+out_put=[]
+if num1>0:
+    out_put.append(num1)
+if num2>0:
+    out_put.append(num2)
+if num3>0:
+    out_put.append(num3)
+print("The output list is ",out_put)
