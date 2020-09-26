39f89ccf68ec1454939f5a06f5d10cdbac7d25cc
vikaskanderai <71124571+vikaskanderai@users.noreply.github.com>
2020-09-26 17:26:01 +0530

    Add files via upload
    through this python program one can save the information details such as name, age, contact number, email-ID

---
+++ student information.py
@@ -0,0 +1,31 @@
+print("Enter student information in the order NAME AGE CONTACT-NUMBER EMAIL-ID")
+
+import csv
+
+def write_into_csv(info_list):
+    with open('student_info.csv', 'a', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        if csv_file.tell()== 0:
+             writer.writerow(["Name","Age","Contact-number","Email-ID"])
+
+
+        writer.writerow(info_list)
+
+
+
+condition=True
+num=1
+while(condition):
+    student_info=input("Enter the information of student number {}:".format(num) )
+   
+    student_info_list= student_info.split(" ")
+    print("The entered information is \nName: {} \nAge: {}\nContact-number: {}\nEmail-ID: {}".format(student_info_list[0],
+                                                                                                     student_info_list[1],student_info_list[2],student_info_list[3]))
+    write_into_csv(student_info_list)
+    condition_check= input("Enter (yes/no) if u want to enter the information of another student: ")
+    if condition_check == "yes":
+        condition=True
+        num=num+1
+    elif condition_check== "no":
+        condition=False
+    
